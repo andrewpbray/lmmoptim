6ce74bf4903aec4a199f65ffb609ffdf84845881
Andrew Bray <andrew.bray@gmail.com>
2015-02-26 10:27:47 -0500

    Change box$bounds from dataframe to matrix to align with output from getbounds()

--- R/mainfunc.R
+++ R/mainfunc.R
@@ -6,7 +6,7 @@ fitlmm <- function ( lines, startbox, eps=0, delE=0, delS=0, M=Inf, maxit=10, ra
 	# don't subdivide boxes whose max RLL is more than M below the MRLE
 	# don't do more than maxit iterations
 	# ratio determines whether the sigsqE and sigsqS conditions are for ratios or absolute differences
-	
+
 	if ( missing ( lines ) ) {
 		print ( "please supply lines" ); return
 	}
@@ -20,7 +20,7 @@ fitlmm <- function ( lines, startbox, eps=0, delE=0, delS=0, M=Inf, maxit=10, ra
 	inactive <- list()
 	ninact <- 0
 	# check whether startbox is a box or a list of boxes
-	ifelse ( length(startbox)==4 && 
+	ifelse ( length(startbox)==4 &&
 	           identical ( names(startbox),
 	                       c("lims.sigsqe", "lims.sigsqs", "status", "bounds")
 	                     ),
@@ -32,32 +32,31 @@ fitlmm <- function ( lines, startbox, eps=0, delE=0, delS=0, M=Inf, maxit=10, ra
 	nact <- length(active)
 	lowbound <- -Inf
 	iter <- 0
-	
+
 	# conditions under which a box becomes inactive
 	killfunc <- function ( box, lb, M, eps, delE, delS, ratio ) {
-		# lb is a lower bound on max logRL; it changes at each iteration
-		# M, eps, delE, delS stay constant throughout the iterations.
-		cond.low <- sum ( box$bounds$upper < lb - M )
-		cond.eps <- sum ( box$bounds$upper - box$bounds$lower ) < eps
-		cond.E <- ifelse ( ratio,
-		                   diff ( log(box$lims.sigsqe) ) < delE,
-		                   diff ( box$lims.sigsqe ) < delE
-		                 )
-		cond.S <- ifelse ( ratio,
-		                   diff ( log(box$lims.sigsqs) ) < delS,
-		                   diff ( box$lims.sigsqs ) < delS
-		                 )
-		return ( cond.low || cond.eps || cond.E || cond.S )
+	  # lb is a lower bound on max logRL; it changes at each iteration
+	  # M, eps, delE, delS stay constant throughout the iterations.
+	  cond.low <- sum ( box$bounds[, 2] < lb - M )
+	  cond.eps <- sum ( box$bounds[, 2] - box$bounds[, 1] ) < eps
+	  cond.E <- ifelse ( ratio,
+	                     diff ( log(box$lims.sigsqe) ) < delE,
+	                     diff ( box$lims.sigsqe ) < delE
+	  )
+	  cond.S <- ifelse ( ratio,
+	                     diff ( log(box$lims.sigsqs) ) < delS,
+	                     diff ( box$lims.sigsqs ) < delS
+	  )
+	  return ( cond.low || cond.eps || cond.E || cond.S )
 	}
 
 	while ( nact > 0 && iter < maxit ) {
-	# Find the lower bound of each box and the maximum of the lower bounds.
-	# For each active box, either make it inactive or divide it.
-		low.act <- max ( vapply ( X = active,
-                                  FUN = function(box) { sum ( box$bounds$lower ) },
-	                              FUN.VALUE = 0.1
-	                            )
-	                   )
+	  # Find the lower bound of each box and the maximum of the lower bounds.
+	  # For each active box, either make it inactive or divide it.
+	  low.act <- max ( vapply ( X = active,
+	                            FUN = function(box) {colSums(box$bounds)[1]},
+	                            FUN.VALUE = 0.1
+	  )
 		lowbound <- max ( lowbound, low.act )
 		kill <- vapply ( X = active,
 	                         FUN = killfunc,
@@ -75,7 +74,7 @@ fitlmm <- function ( lines, startbox, eps=0, delE=0, delS=0, M=Inf, maxit=10, ra
 		}
 		active <- kids
 		nact <- length(active)
-	
+
 		iter <- iter+1
 		write ( c ( "iteration", iter, "nact", nact, "ninact", ninact, "lowbound", lowbound ),
 	            file="bigcode.out", ncolumns=8, append=TRUE
@@ -84,12 +83,11 @@ fitlmm <- function ( lines, startbox, eps=0, delE=0, delS=0, M=Inf, maxit=10, ra
 
 
 	tmp <- t ( vapply ( X = c ( active, inactive ),
-                        FUN = function(box) { c ( box$lims.sigsqs,
-                              	                  box$lims.sigsqe,
-                              	                  sum ( box$bounds$lower ),
-                              	                  sum ( box$bounds$upper )
-                              	                )
-                              	            },
+	                    FUN = function(box) { c ( box$lims.sigsqs,
+	                                              box$lims.sigsqe,
+	                                              colSums(box$bounds)
+	                                              )
+	                      },
                         FUN.VALUE = c ( sigsqs.lo = 0.1,
                                         sigsqs.hi = 0.1,
                                         sigsqe.lo = 0.1,
