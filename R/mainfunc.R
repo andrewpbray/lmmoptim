9ed0dfec4acf52cca26cbf61c72e51d2a3165b9a
Andrew Bray <andrew.bray@gmail.com>
2015-02-27 15:14:57 -0500

    Add runtime output to .out in fitlmm()

--- R/mainfunc.R
+++ R/mainfunc.R
@@ -1,44 +1,45 @@
-fitlmm <- function(lines, startbox, eps = 0, delE = 0, delS = 0, M = Inf, maxit = 10, 
-    ratio = FALSE) {
+fitlmm <- function(lines, startbox, eps = 0, delE = 0, delS = 0, M = Inf, maxit = 10,
+                   ratio = FALSE, lognote = "summary") {
     # five control settings don't subdivide boxes whose upper and lower bounds
     # are within eps don't subdivide boxes whose sigsqE spans less than delE
     # don't subdivide boxes whose sigsqS spans less than delS don't subdivide
     # boxes whose max RLL is more than M below the MRLE don't do more than maxit
     # iterations ratio determines whether the sigsqE and sigsqS conditions are
     # for ratios or absolute differences
-    
+    start_time <- Sys.time()
+
     if (missing(lines)) {
         print("please supply lines")
         return
     }
     if (missing(startbox)) {
-        startbox <- makebox(lines = lines, lims.sigsqs = c(0, max(lines$int.sigsqs[is.finite(lines$int.sigsqs)])), 
-            lims.sigsqe = c(0, max(lines$int.sigsqe[is.finite(lines$int.sigsqe)])), 
+        startbox <- makebox(lines = lines, lims.sigsqs = c(0, max(lines$int.sigsqs[is.finite(lines$int.sigsqs)])),
+            lims.sigsqe = c(0, max(lines$int.sigsqe[is.finite(lines$int.sigsqe)])),
             status = rep("straddle", nrow(lines)))
     }
     inactive <- list()
     ninact <- 0
     # check whether startbox is a box or a list of boxes
-    active <- ifelse(length(startbox) == 4 && identical(names(startbox), c("lims.sigsqe", 
+    active <- ifelse(length(startbox) == 4 && identical(names(startbox), c("lims.sigsqe",
         "lims.sigsqs", "status", "bounds")), list(startbox), startbox)
     # active <- list() active[[1]] <- startbox
     nact <- length(active)
     lowbound <- -Inf
     iter <- 0
-    
+
     # conditions under which a box becomes inactive
     killfunc <- function(box, lb, M, eps, delE, delS, ratio) {
         # lb is a lower bound on max logRL; it changes at each iteration M, eps,
         # delE, delS stay constant throughout the iterations.
         cond.low <- sum(box$bounds[, 2]) < lb - M
         cond.eps <- sum(box$bounds[, 2] - box$bounds[, 1]) < eps
-        cond.E <- ifelse(ratio, diff(log(box$lims.sigsqe)) < delE, diff(box$lims.sigsqe) < 
+        cond.E <- ifelse(ratio, diff(log(box$lims.sigsqe)) < delE, diff(box$lims.sigsqe) <
             delE)
-        cond.S <- ifelse(ratio, diff(log(box$lims.sigsqs)) < delS, diff(box$lims.sigsqs) < 
+        cond.S <- ifelse(ratio, diff(log(box$lims.sigsqs)) < delS, diff(box$lims.sigsqs) <
             delS)
         return(cond.low || cond.eps || cond.E || cond.S)
     }
-    
+
     while (nact > 0 && iter < maxit) {
         # Find the lower bound of each box and the maximum of the lower bounds.  For
         # each active box, either make it inactive or divide it.
@@ -46,7 +47,7 @@ fitlmm <- function(lines, startbox, eps = 0, delE = 0, delS = 0, M = Inf, maxit 
             colSums(box$bounds)[1]
         }, FUN.VALUE = 0.1))
         lowbound <- max(lowbound, low.act)
-        kill <- vapply(X = active, FUN = killfunc, FUN.VALUE = TRUE, lb = lowbound, 
+        kill <- vapply(X = active, FUN = killfunc, FUN.VALUE = TRUE, lb = lowbound,
             M = M, eps = eps, delE - delE, delS = delS, ratio = ratio)
         nkill <- sum(kill)
         if (nkill > 0) {
@@ -61,16 +62,19 @@ fitlmm <- function(lines, startbox, eps = 0, delE = 0, delS = 0, M = Inf, maxit 
         }
         active <- kids
         nact <- length(active)
-        
+
         iter <- iter + 1
-        write(c("iteration", iter, "nact", nact, "ninact", ninact, "lowbound", 
+        write(c("iteration", iter, "nact", nact, "ninact", ninact, "lowbound",
             lowbound), file = "bigcode.out", ncolumns = 8, append = TRUE)
     }
-    
-    
+
+
     tmp <- t(vapply(X = c(active, inactive), FUN = function(box) {
         c(box$lims.sigsqs, box$lims.sigsqe, colSums(box$bounds))
-    }, FUN.VALUE = c(sigsqs.lo = 0.1, sigsqs.hi = 0.1, sigsqe.lo = 0.1, sigsqe.hi = 0.1, 
+    }, FUN.VALUE = c(sigsqs.lo = 0.1, sigsqs.hi = 0.1, sigsqe.lo = 0.1, sigsqe.hi = 0.1,
         rll.lower = 0.1, rll.upper = 0.1)))
+    end_time <- Sys.time()
+    write(paste(log_note, "runtime: ", round(end_time - start_time, digits = 3)),
+          file = "bigcode.out", ncolumns = 1, append = TRUE)
     return(data.frame(tmp))
-} 
+}
