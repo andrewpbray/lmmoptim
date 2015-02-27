6e4f7dc2db362a955409875e060b43f81b5fc1d7
Andrew Bray <andrew.bray@gmail.com>
2015-02-27 10:18:05 -0500

    Apply formatR to bring code into style compliance

--- R/linefuncs.R
+++ R/linefuncs.R
@@ -1,95 +1,98 @@
 # Given X, Z, Y, find the constants in Hodges' reexpression
 
-findlines <- function ( x, z, y, SigE, SigS ){
-	# Find the constants in Hodges' reexpression.
-	# Just those constants related to the model, not the prior.
-	# sanity checks
-	if  ( !is.matrix(x) ) print ("x should be a matrix")
-	if  ( !is.matrix(z) ) print ("z should be a matrix")
-	if  ( !is.vector(y) ) print ("y should be a vector")
-	
-	n <- length(y)
-	nx <- ncol(x)
-	nz <- ncol(z)
-	
-	if ( nrow(x) != n ) print ("nrow(x) != length(y)")
-	if ( nrow(z) != n ) print ("nrow(z) != length(y)")
-	
-	if  ( !is.matrix(SigE) ) print ("SigE should be a matrix")
-	if  ( !is.matrix(SigS) ) print ("SigS should be a matrix")
-	if ( nrow(SigE) != n || ncol(SigE) !=n )
-	  print ("SigE should be a square matrix to match length(y)")
-	if ( nrow(SigS) != nz || ncol(SigS) != nz )
-	  print ("SigS should be a square matrix to match ncol(z)")
-	
-	# Is SigE the identity?  If not, make it so.
-	if ( !identical (SigE, diag(n) ) ){
-		tmp <- inv.sqrt(SigE)
-		y <- tmp %*% y
-		x <- tmp %*% x
-		z <- tmp %*% z
-		SigE <- diag(n)
-	}
-
-	# Is SigS the identity?  If not, make it so.
-	if ( !identical ( SigS, diag(nz) ) ){
-		tmp <- sqrt.m(SigS)
-		z  <- z %*% tmp
-		SigS <- diag(nz)
-	}
-	
-	# sx, sz, Gamma_x, Gamma_z, Gamma_c # Is Gamma_c really needed?
-	qrx <- qr (x, LAPACK=FALSE ) # use LINPACK to get rank(x)
-	sx <- qrx$rank
-	Gamx <- qr.Q(qrx)[,1:sx]
-	if ( sx == 1 ) Gamx <- matrix ( Gamx, ncol=1 )
-	
-	tmp <- qr.resid ( qrx, z )
-	qrz <- qr (tmp, LAPACK=FALSE ) # use LINPACK to get rank(x)
-	sz <- qrz$rank
-	Gamz <- qr.Q(qrz)[,1:sz]
-	if ( sz == 1 ) Gamz <- matrix ( Gamz, ncol=1 )
-	
-	M <- qr.solve ( cbind(Gamx,Gamz), cbind(x,z) )
-	M.zz <- M [ -(1:sx), -(1:ncol(x)), drop=FALSE ]
-	tmp <- svd(M.zz)
-	a <- tmp$d^2 # follows Eq (15.1)
-	v <- t(tmp$u) %*% t(Gamz) %*% y # follows Eq (15.4)
-	if ( length(a) != sz ) print ( "length(a) != sz" )
-	if ( length(v) != sz ) print ( "length(v) != sz" )
-	rss <- sum ( resid ( lm ( y ~ cbind(Gamx,Gamz) ) )^2 )
-
-  lines <- data.frame (
-    a = c ( a, 0 ),
-    v = c ( v, sqrt(rss) ),
-    int.sigsqs = c ( v^2/a, NA ),  
-    int.sigsqe = c ( v^2, rss/(n-(sx+sz)) ),
-    slope = c ( -1/a, -Inf ),
-    multiplier.log = c ( rep(1,sz), n-(sx+sz) ),
-    multiplier.inv = c ( v^2, rss )
-  )
-  return(lines)
+findlines <- function(x, z, y, SigE, SigS) {
+    # Find the constants in Hodges' reexpression.  Just those constants related
+    # to the model, not the prior.  sanity checks
+    if (!is.matrix(x)) 
+        print("x should be a matrix")
+    if (!is.matrix(z)) 
+        print("z should be a matrix")
+    if (!is.vector(y)) 
+        print("y should be a vector")
+    
+    n <- length(y)
+    nx <- ncol(x)
+    nz <- ncol(z)
+    
+    if (nrow(x) != n) 
+        print("nrow(x) != length(y)")
+    if (nrow(z) != n) 
+        print("nrow(z) != length(y)")
+    
+    if (!is.matrix(SigE)) 
+        print("SigE should be a matrix")
+    if (!is.matrix(SigS)) 
+        print("SigS should be a matrix")
+    if (nrow(SigE) != n || ncol(SigE) != n) 
+        print("SigE should be a square matrix to match length(y)")
+    if (nrow(SigS) != nz || ncol(SigS) != nz) 
+        print("SigS should be a square matrix to match ncol(z)")
+    
+    # Is SigE the identity?  If not, make it so.
+    if (!identical(SigE, diag(n))) {
+        tmp <- inv.sqrt(SigE)
+        y <- tmp %*% y
+        x <- tmp %*% x
+        z <- tmp %*% z
+        SigE <- diag(n)
+    }
+    
+    # Is SigS the identity?  If not, make it so.
+    if (!identical(SigS, diag(nz))) {
+        tmp <- sqrt.m(SigS)
+        z <- z %*% tmp
+        SigS <- diag(nz)
+    }
+    
+    # sx, sz, Gamma_x, Gamma_z, Gamma_c # Is Gamma_c really needed?
+    qrx <- qr(x, LAPACK = FALSE)  # use LINPACK to get rank(x)
+    sx <- qrx$rank
+    Gamx <- qr.Q(qrx)[, 1:sx]
+    if (sx == 1) 
+        Gamx <- matrix(Gamx, ncol = 1)
+    
+    tmp <- qr.resid(qrx, z)
+    qrz <- qr(tmp, LAPACK = FALSE)  # use LINPACK to get rank(x)
+    sz <- qrz$rank
+    Gamz <- qr.Q(qrz)[, 1:sz]
+    if (sz == 1) 
+        Gamz <- matrix(Gamz, ncol = 1)
+    
+    M <- qr.solve(cbind(Gamx, Gamz), cbind(x, z))
+    M.zz <- M[-(1:sx), -(1:ncol(x)), drop = FALSE]
+    tmp <- svd(M.zz)
+    a <- tmp$d^2  # follows Eq (15.1)
+    v <- t(tmp$u) %*% t(Gamz) %*% y  # follows Eq (15.4)
+    if (length(a) != sz) 
+        print("length(a) != sz")
+    if (length(v) != sz) 
+        print("length(v) != sz")
+    rss <- sum(resid(lm(y ~ cbind(Gamx, Gamz)))^2)
+    
+    lines <- data.frame(a = c(a, 0), v = c(v, sqrt(rss)), int.sigsqs = c(v^2/a, 
+        NA), int.sigsqe = c(v^2, rss/(n - (sx + sz))), slope = c(-1/a, -Inf), 
+        multiplier.log = c(rep(1, sz), n - (sx + sz)), multiplier.inv = c(v^2, 
+            rss))
+    return(lines)
 }
 
 
-addprior <- function ( lines, a.E=0, b.E=0, a.S=0, b.S=0 ){
-	if ( (a.E != 0)  ||  (b.E != 0) ) { # if sigesq ~ IG(a.E,b.E)
-			vert <- which ( lines$slope == -Inf )
-			lines$multiplier.log[vert] <- lines$multiplier.log[vert] + 2*(a.E+1)
-			lines$multiplier.inv[vert] <- lines$multiplier.inv[vert] + b.E
-			lines$v[vert] <- sqrt(lines$multiplier.inv[vert])
-			lines$int.sigsqe[vert] <- lines$multiplier.inv[vert] / lines$multiplier.log[vert]
-	}
-	if ( (a.S != 0)  ||  (b.S != 0) ) { # if sigssq ~ IG(a.S,b.S)
-		horiz <- data.frame ( a = 1, 
-		                      multiplier.inv = 2*b.S,
-		                      multiplier.log = 2*(a.S+1),
-		                      v = sqrt(2*b.S),
-		                      int.sigsqs = b.S / (a.S+1),
-		                      int.sigsqe = NA,
-		                      slope = 0
-		                    )
-		lines <- rbind ( lines, horiz )
-	}
-	return ( lines )
-}
+addprior <- function(lines, a.E = 0, b.E = 0, a.S = 0, b.S = 0) {
+    if ((a.E != 0) || (b.E != 0)) {
+        # if sigesq ~ IG(a.E,b.E)
+        vert <- which(lines$slope == -Inf)
+        lines$multiplier.log[vert] <- lines$multiplier.log[vert] + 2 * (a.E + 
+            1)
+        lines$multiplier.inv[vert] <- lines$multiplier.inv[vert] + b.E
+        lines$v[vert] <- sqrt(lines$multiplier.inv[vert])
+        lines$int.sigsqe[vert] <- lines$multiplier.inv[vert]/lines$multiplier.log[vert]
+    }
+    if ((a.S != 0) || (b.S != 0)) {
+        # if sigssq ~ IG(a.S,b.S)
+        horiz <- data.frame(a = 1, multiplier.inv = 2 * b.S, multiplier.log = 2 * 
+            (a.S + 1), v = sqrt(2 * b.S), int.sigsqs = b.S/(a.S + 1), int.sigsqe = NA, 
+            slope = 0)
+        lines <- rbind(lines, horiz)
+    }
+    return(lines)
+} 
--- R/mainfunc.R
+++ R/mainfunc.R
@@ -1,102 +1,76 @@
-fitlmm <- function ( lines, startbox, eps=0, delE=0, delS=0, M=Inf, maxit=10, ratio=FALSE  ) {
-	# five control settings
-	# don't  subdivide boxes whose upper and lower bounds are within eps
-	# don't subdivide boxes whose sigsqE spans less than delE
-	# don't subdivide boxes whose sigsqS spans less than delS
-	# don't subdivide boxes whose max RLL is more than M below the MRLE
-	# don't do more than maxit iterations
-	# ratio determines whether the sigsqE and sigsqS conditions are for ratios or absolute differences
-
-	if ( missing ( lines ) ) {
-		print ( "please supply lines" ); return
-	}
-	if ( missing ( startbox ) ) {
-		startbox <- makebox ( lines = lines,
-		                      lims.sigsqs = c ( 0, max ( lines$int.sigsqs[is.finite(lines$int.sigsqs)] ) ),
-	                        lims.sigsqe = c ( 0, max ( lines$int.sigsqe[is.finite(lines$int.sigsqe)] ) ),
-	                        status = rep ( "straddle", nrow(lines) )
-		                      )
-	}
-	inactive <- list()
-	ninact <- 0
-	# check whether startbox is a box or a list of boxes
-	active <- ifelse ( length(startbox)==4 &&
-	           identical ( names(startbox),
-	                       c("lims.sigsqe", "lims.sigsqs", "status", "bounds")
-	                     ),
-	           list(startbox),
-	           startbox
-	       )
-	#active <- list()
-	#active[[1]] <- startbox
-	nact <- length(active)
-	lowbound <- -Inf
-	iter <- 0
-
-	# conditions under which a box becomes inactive
-	killfunc <- function ( box, lb, M, eps, delE, delS, ratio ) {
-	  # lb is a lower bound on max logRL; it changes at each iteration
-	  # M, eps, delE, delS stay constant throughout the iterations.
-	  cond.low <- sum ( box$bounds[, 2]) < lb - M
-	  cond.eps <- sum ( box$bounds[, 2] - box$bounds[, 1] ) < eps
-	  cond.E <- ifelse ( ratio,
-	                     diff ( log(box$lims.sigsqe) ) < delE,
-	                     diff ( box$lims.sigsqe ) < delE
-	  )
-	  cond.S <- ifelse ( ratio,
-	                     diff ( log(box$lims.sigsqs) ) < delS,
-	                     diff ( box$lims.sigsqs ) < delS
-	  )
-	  return ( cond.low || cond.eps || cond.E || cond.S )
-	}
-
-	while ( nact > 0 && iter < maxit ) {
-	  # Find the lower bound of each box and the maximum of the lower bounds.
-	  # For each active box, either make it inactive or divide it.
-	  low.act <- max ( vapply ( X = active,
-	                            FUN = function(box) {colSums(box$bounds)[1]},
-	                            FUN.VALUE = 0.1
-	                            ))
-		lowbound <- max ( lowbound, low.act )
-		kill <- vapply ( X = active,
-	                         FUN = killfunc,
-	                         FUN.VALUE = TRUE,
-	                         lb=lowbound, M=M,eps=eps, delE-delE, delS=delS, ratio=ratio
-	                   )
-		nkill <- sum(kill)
-		if ( nkill > 0 )  { inactive [ (ninact+1) : (ninact+nkill) ] <- active[kill] }
-		ninact <- length(inactive)
-		kids <- list()
-		nkids <- 0
-		for ( i in which(!kill) ) {
-			kids [ (nkids+1):(nkids+4) ] <- splitbox ( active[[i]], lines ) # boxes are split into 4 parts
-			nkids <- nkids + 4
-		}
-		active <- kids
-		nact <- length(active)
-
-		iter <- iter+1
-		write ( c ( "iteration", iter, "nact", nact, "ninact", ninact, "lowbound", lowbound),
-	            file="bigcode.out", ncolumns=8, append=TRUE
-	          )
-	}
-
-
-	tmp <- t ( vapply ( X = c ( active, inactive ),
-	                    FUN = function(box) { c ( box$lims.sigsqs,
-	                                              box$lims.sigsqe,
-	                                              colSums(box$bounds)
-	                                              )
-	                      },
-                        FUN.VALUE = c ( sigsqs.lo = 0.1,
-                                        sigsqs.hi = 0.1,
-                                        sigsqe.lo = 0.1,
-                                        sigsqe.hi = 0.1,
-                                        rll.lower = 0.1,
-                                        rll.upper = 0.1
-                                      )
-                      )
-                )
-	return ( data.frame ( tmp ) )
-}
-
+fitlmm <- function(lines, startbox, eps = 0, delE = 0, delS = 0, M = Inf, maxit = 10, 
+    ratio = FALSE) {
+    # five control settings don't subdivide boxes whose upper and lower bounds
+    # are within eps don't subdivide boxes whose sigsqE spans less than delE
+    # don't subdivide boxes whose sigsqS spans less than delS don't subdivide
+    # boxes whose max RLL is more than M below the MRLE don't do more than maxit
+    # iterations ratio determines whether the sigsqE and sigsqS conditions are
+    # for ratios or absolute differences
+    
+    if (missing(lines)) {
+        print("please supply lines")
+        return
+    }
+    if (missing(startbox)) {
+        startbox <- makebox(lines = lines, lims.sigsqs = c(0, max(lines$int.sigsqs[is.finite(lines$int.sigsqs)])), 
+            lims.sigsqe = c(0, max(lines$int.sigsqe[is.finite(lines$int.sigsqe)])), 
+            status = rep("straddle", nrow(lines)))
+    }
+    inactive <- list()
+    ninact <- 0
+    # check whether startbox is a box or a list of boxes
+    active <- ifelse(length(startbox) == 4 && identical(names(startbox), c("lims.sigsqe", 
+        "lims.sigsqs", "status", "bounds")), list(startbox), startbox)
+    # active <- list() active[[1]] <- startbox
+    nact <- length(active)
+    lowbound <- -Inf
+    iter <- 0
+    
+    # conditions under which a box becomes inactive
+    killfunc <- function(box, lb, M, eps, delE, delS, ratio) {
+        # lb is a lower bound on max logRL; it changes at each iteration M, eps,
+        # delE, delS stay constant throughout the iterations.
+        cond.low <- sum(box$bounds[, 2]) < lb - M
+        cond.eps <- sum(box$bounds[, 2] - box$bounds[, 1]) < eps
+        cond.E <- ifelse(ratio, diff(log(box$lims.sigsqe)) < delE, diff(box$lims.sigsqe) < 
+            delE)
+        cond.S <- ifelse(ratio, diff(log(box$lims.sigsqs)) < delS, diff(box$lims.sigsqs) < 
+            delS)
+        return(cond.low || cond.eps || cond.E || cond.S)
+    }
+    
+    while (nact > 0 && iter < maxit) {
+        # Find the lower bound of each box and the maximum of the lower bounds.  For
+        # each active box, either make it inactive or divide it.
+        low.act <- max(vapply(X = active, FUN = function(box) {
+            colSums(box$bounds)[1]
+        }, FUN.VALUE = 0.1))
+        lowbound <- max(lowbound, low.act)
+        kill <- vapply(X = active, FUN = killfunc, FUN.VALUE = TRUE, lb = lowbound, 
+            M = M, eps = eps, delE - delE, delS = delS, ratio = ratio)
+        nkill <- sum(kill)
+        if (nkill > 0) {
+            inactive[(ninact + 1):(ninact + nkill)] <- active[kill]
+        }
+        ninact <- length(inactive)
+        kids <- list()
+        nkids <- 0
+        for (i in which(!kill)) {
+            kids[(nkids + 1):(nkids + 4)] <- splitbox(active[[i]], lines)  # boxes are split into 4 parts
+            nkids <- nkids + 4
+        }
+        active <- kids
+        nact <- length(active)
+        
+        iter <- iter + 1
+        write(c("iteration", iter, "nact", nact, "ninact", ninact, "lowbound", 
+            lowbound), file = "bigcode.out", ncolumns = 8, append = TRUE)
+    }
+    
+    
+    tmp <- t(vapply(X = c(active, inactive), FUN = function(box) {
+        c(box$lims.sigsqs, box$lims.sigsqe, colSums(box$bounds))
+    }, FUN.VALUE = c(sigsqs.lo = 0.1, sigsqs.hi = 0.1, sigsqe.lo = 0.1, sigsqe.hi = 0.1, 
+        rll.lower = 0.1, rll.upper = 0.1)))
+    return(data.frame(tmp))
+} 
