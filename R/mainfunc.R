6e3c0c05f7b6593e3faa6167a2b0d087b95a32d3
Andrew Bray <andrewbray@gmail.com>
2015-05-11 13:11:25 -0400

    Add Roxygen documentation.

--- R/mainfunc.R
+++ R/mainfunc.R
@@ -1,11 +1,54 @@
+#' Learn RLL or Log-posterior for two variance MM.
+#'
+#' Learns the shape of the objective function within epsilon.
+#'
+#' This is the primary function that implements the branch-bound-kill
+#' algorithm described in "Approximately Exact Calculations for Linear
+#' Mixed Models" by Lavine & Hodges (2015). This an interative algorithm
+#' that can require substantial computation time. It is recommended that
+#' the user start with a conservative \code{maxit} and do additional
+#' computations as necessary.
+#'
+#' The arguments of this function include five settings to control when
+#' to stop branching (subdividing) a box:
+#' \enumerate{
+#'   \item when the upper and lower bounds are within \code{eps} of one another.
+#'   \item when the width of \eqn{\sigma^2_e} is less than \code{delE}.
+#'   \item when the width of \eqn{\sigma^2_s} is less than \code{delS}.
+#'   \item when the upper bound is more than \code{M} below the highest global
+#'   lower bound.
+#'   \item when the number of iterations reaches \code{maxit}.
+#'   }
+#'
+#' @param lines a dataframe that contains the constants that define the
+#'   line represented by each term in the sum. Created as output from
+#'   \code{\link{findlines}}.
+#' @param startbox a list of boxes and their bounds, possibly the output
+#'   from a previous call to \code{fitlmm}. If left empty, will create
+#'   a new startbox from a call to \code{makebox}.
+#' @param eps a non-negative numeric indicating the tolerance within which you
+#'   would like to learn the function. Default is 0.
+#' @param delE a non-negative numeric indicating the width of \eqn{\sigma^2_e}
+#'   beyond which the algorithm will stop branching boxes. Default is 0.
+#' @param delS a non-negative numeric indicating the width of \eqn{\sigma^2_s}
+#'   beyond which the algorithm will stop branching boxes. Default is 0.
+#' @param M a non-negative numeric indicating the size of the buffer between
+#'   the highest global lower bound and the upper bound of a given box past
+#'   that box will now be branched further.  Default is \code{Inf}.
+#' @param maxit a positive integer indicating the maximum number of iterations
+#'   of the algorithm.  Default is 10.
+#' @param ratio a logical indicating if \code{delE} and \code{delS} are specified as
+#'   ZZQ.  Default is \code{FALSE}.
+#' @param lognote a string to append to the "bigcode.out" log file to annotate
+#'   the purpose of each run of the algorithm. For use in benchmarking computation
+#'   time. Default is \code{"summary"}.
+#'
+#' @return a list of boxes including their limits in \eqn{\sigma^2_e} and
+#'   \eqn{\sigma^2_e} as well as their bounds.
+
 fitlmm <- function(lines, startbox, eps = 0, delE = 0, delS = 0, M = Inf, maxit = 10,
                    ratio = FALSE, lognote = "summary") {
-    # five control settings don't subdivide boxes whose upper and lower bounds
-    # are within eps don't subdivide boxes whose sigsqE spans less than delE
-    # don't subdivide boxes whose sigsqS spans less than delS don't subdivide
-    # boxes whose max RLL is more than M below the MRLE don't do more than maxit
-    # iterations ratio determines whether the sigsqE and sigsqS conditions are
-    # for ratios or absolute differences
+
     start_time <- Sys.time()
 
     if (missing(lines)) {
@@ -22,7 +65,6 @@ fitlmm <- function(lines, startbox, eps = 0, delE = 0, delS = 0, M = Inf, maxit 
     # check whether startbox is a box or a list of boxes
     active <- ifelse(length(startbox) == 4 && identical(names(startbox), c("lims.sigsqe",
         "lims.sigsqs", "status", "bounds")), list(startbox), startbox)
-    # active <- list() active[[1]] <- startbox
     nact <- length(active)
 
     lowbound <- -Inf
@@ -30,7 +72,7 @@ fitlmm <- function(lines, startbox, eps = 0, delE = 0, delS = 0, M = Inf, maxit 
 
     # conditions under which a box becomes inactive
     killfunc <- function(box, lb, M, eps, delE, delS, ratio) {
-        # lb is a lower bound on max logRL; it changes at each iteration M, eps,
+        # lb is a lower bound on max f; it changes at each iteration. M, eps,
         # delE, delS stay constant throughout the iterations.
         cond.low <- box$bounds[2] < lb - M
         cond.eps <- diff(box$bounds) < eps
