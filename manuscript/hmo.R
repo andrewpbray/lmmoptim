c75f0bc447444ef6c3365b1a962a4b9c33c84bbe
Andrew Bray <andrewbray@gmail.com>
2015-05-12 12:29:50 -0400

    Change spacing style.

--- manuscript/hmo.R
+++ manuscript/hmo.R
@@ -27,12 +27,11 @@ showlines(lines)
 dev.off()
 
 mlrebox <- with(lines,
-                makebox(lines = lines,
-                        lims.sigsqs = c(0, max(int.sigsqs[is.finite(int.sigsqs)])),
+                makebox(lims.sigsqs = c(0, max(int.sigsqs[is.finite(int.sigsqs)])),
 	                      lims.sigsqe = c(0, max(int.sigsqe[is.finite(int.sigsqe)])),
-	                      status = rep("straddle", nrow(lines))
-	                      )
-                )
+	                      status = rep("straddle", nrow(lines)),
+                        lines = lines))
+
 boxes <- fitlmm(lines = lines, mlrebox, eps = 1, delE = 10, delS = 10, M = 5, maxit = 10)
 p <- showboxes(boxes)
 
@@ -53,15 +52,13 @@ dev.off()
 
 # the previous figure suggests we can limit our attention to a smaller region
 box2 <- with(lines,
-             makebox(lines = lines,
-                     lims.sigsqe = c(0, 1000),
+             makebox(lims.sigsqe = c(0, 1000),
                      lims.sigsqs = c(0, 2000),
-                     status = rep("straddle", nrow(lines))
-                     )
-             )
+                     status = rep("straddle", nrow(lines)),
+                     lines = lines))
+
 boxes2 <- fitlmm(lines = lines, box2, eps = .5, delE = 0, delS = 0, ratio = TRUE,
-                 M = 20, maxit = 25
-                 )
+                 M = 20, maxit = 25)
 
 mrle <- which.max(boxes2$rll.lower)
 
@@ -83,12 +80,10 @@ dev.off()
 
 p + scale_x_log10(limits = c(200, 1000),
                   breaks = seq(200, 1000, by = 200),
-                  name = expression(sigma^2[e])
-                  ) +
+                  name = expression(sigma^2[e])) +
   scale_y_log10(limits = c(0, 1000),
                 breaks = 10^seq(-3, 3, by = 2),
-                name = expression(sigma^2[s])
-                ) +
+                name = expression(sigma^2[s])) +
   theme_bw()
 
 save.image()
@@ -119,16 +114,15 @@ showlines(lines)
 dev.off()
 
 mlrebox <- with(lines,
-                makebox(lines = lines,
-                        lims.sigsqs = c(0, max(int.sigsqs[is.finite(int.sigsqs)])),
+                makebox(lims.sigsqs = c(0, max(int.sigsqs[is.finite(int.sigsqs)])),
 	                      lims.sigsqe = c(0, max(int.sigsqe[is.finite(int.sigsqe)])),
-	                      status = rep("straddle", nrow(lines))
-	                      )
-	            )
+	                      status = rep("straddle", nrow(lines)),
+                        lines = lines))
+
 boxes.HH11 <- fitlmm(lines = lines, mlrebox, eps = 5,
                      delE = log(10), delS = log(10), ratio = TRUE,
-                     M = 5, maxit = 10
-                     )
+                     M = 5, maxit = 10)
+
 p <- showboxes(boxes.HH11)
 pdf("manuscript/figs/hmo.HH11.boxes.pdf")
 p + scale_x_continuous(name = expression(sigma[e]^2)) +
@@ -139,23 +133,20 @@ dev.off()
 p <- showfunc(boxes.HH11)
 pdf("manuscript/figs/hmo.HH11.rll.pdf")
 p + scale_x_log10(name = expression(sigma[e]^2),
-                  breaks = c(100,300,1000,3000,10000)
-                  ) +
+                  breaks = c(100,300,1000,3000,10000)) +
     scale_y_log10(name = expression(sigma[s]^2),
-                  breaks = c(30,100,300,1000,3000,10000)
-                  ) +
+                  breaks = c(30,100,300,1000,3000,10000)) +
     theme_bw()
 dev.off()
 
 # Run the algorithm again, with input refined as suggested by
 # the previous run
 box2 <- with(lines,
-             makebox(lines = lines,
-	                   lims.sigsqe = c(300, 1000),
+             makebox(lims.sigsqe = c(300, 1000),
                      lims.sigsqs = c(0, 1000),
-	                   status = rep("straddle", nrow(lines))
-	                   )
-	         )
+	                   status = rep("straddle", nrow(lines)),
+                     lines = lines))
+
 boxes.HH11 <- fitlmm(lines = lines, box2, eps = 1, M = 10, maxit = 15)
 
 p <- showboxes(boxes.HH11)
@@ -168,11 +159,9 @@ dev.off()
 p <- showfunc(boxes.HH11)
 pdf("manuscript/figs/hmo.HH11.rll2.pdf")
 p + scale_x_log10(name = expression(sigma[e]^2),
-                  breaks = c (300,500,1000)
-                  ) +
+                  breaks = c (300,500,1000)) +
     scale_y_log10(name = expression(sigma[s]^2),
-                  breaks = c(1, 3, 10, 30, 100, 300, 1000)
-                  ) +
+                  breaks = c(1, 3, 10, 30, 100, 300, 1000)) +
     theme_bw()
 dev.off()
 
@@ -194,19 +183,17 @@ dev.off()
 p <- showfunc(boxes.HH11Bayes)
 jpeg("manuscript/figs/hmo_HH11Bayes_rll.jpg")
 p + scale_x_log10(name = expression(sigma[e]^2),
-                  breaks = c(300, 500, 1000)
-                  ) +
+                  breaks = c(300, 500, 1000)) +
     scale_y_log10(name = expression(sigma[s]^2),
                     # breaks = c (1, 3, 10, 30,100,300,1000)
-                  breaks = 10^seq(-3, 3, by = 1)
-                  ) +
+                  breaks = 10^seq(-3, 3, by = 1)) +
     theme_bw()
 dev.off()
 
 # The figure reveals a ridge.  Let's investigate
 ridge <- with(boxes.HH11Bayes,
-              rll.upper > max(rll.lower) & (rll.upper-rll.lower) > 5
-              )
+              rll.upper > max(rll.lower) & (rll.upper-rll.lower) > 5)
+
 with(boxes.HH11Bayes, range(sigsqs.lo[good]))
 with(boxes.HH11Bayes, range(sigsqs.hi[good]))
 with(boxes.HH11Bayes, range(rll.upper[good]))
